50fdba73862292b3859ad88896e4d43555047e5b
akil1me <akipax123@gmail.com>
2022-08-10 14:54:12 +0500

    Fixed item to for

--- js/script.js
+++ js/script.js
@@ -2,19 +2,20 @@ let elForm = document.querySelector(".hero__form");
 let elInput = elForm.querySelector(".hero__input");
 let elList = document.querySelector(".hero__list");
 
+let arr = [];
+console.log(arr);
+
 elForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
 
   let inputValue = elInput.value;
   let elListItem = document.createElement("li");
+  arr.push(elInput.value);
 
   if (isNaN(inputValue)) {
-    for (arr of inputValue) {
-      let arr = [];
+    for (item of arr) {
       elList.appendChild(elListItem);
-      arr.push(elInput.value);
-      elListItem.textContent = arr;
-      break;
+      elListItem.textContent = item;
     }
   } else {
     alert("Iltimos Son kiritmang");
